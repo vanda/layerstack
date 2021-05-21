7950d01f96b944f2e4ef40e9d6489219eee4aba2
methodog <sittingbottom@gmail.com>
2021-05-21 18:21:22 +0100

    demo update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,14 @@
 import layerStacker, { LayerStack } from './layerstack';
 
-Array.from(document.querySelectorAll('.layerstack'), (layerstackEl) => {
-  layerStacker(layerstackEl);
+/* for purposes of this demo, allow manifest to be set by an optional querystring */
+const qstr = new Map(location.search.slice(1).split('&').map(kv => kv.split('=')));
+if (qstr.has('manifest')) {
+  document.querySelector('.layerstack').dataset.iiifManifest = qstr.get('manifest');
+}
+
+/* activate any layerstack elements in the page */
+document.addEventListener('DOMContentLoaded', () => {
+  Array.from(document.querySelectorAll('.layerstack'), (layerstackEl) => {
+    layerStacker(layerstackEl);
+  });
 });
